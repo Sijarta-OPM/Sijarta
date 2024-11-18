9a1ef6c6de74efa5a22bfe9bf4b322e7a49e397a
Adam <adamsembiring01@gmail.com>
2024-11-18 15:55:21 +0700

    [FEAT] initial app

--- sijarta/urls.py
+++ sijarta/urls.py
@@ -5,6 +5,5 @@ from authentication.views import login_view  # Impor login_view dari authenticat
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-    path('', include('biru.urls')),
     path('', login_view, name='home'),  # Menambahkan URL root yang mengarah ke login_view
 ]
