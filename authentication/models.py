5120c64174866b42aead961b141304790368bc45
RayhanSP <ray.han2005.rh@gmail.com>
2024-11-18 18:40:26 +0700

    fix autisme git stash

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userdata')
+    no_hp = models.CharField(max_length=15, unique=True)
+    alamat = models.TextField(blank=True, null=True)
+    tanggal_lahir = models.DateField(blank=True, null=True)
+    role = models.CharField(
+        max_length=10,
+        choices=[('PENGGUNA', 'Pengguna'), ('PEKERJA', 'Pekerja')],
+        default='PENGGUNA'
+    )
